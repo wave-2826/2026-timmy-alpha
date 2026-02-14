e33165be69a1186a5ee8fb77201a24317f905e01
2826WaveRobotics <wavesoftware@waverobotics.com>
2026-02-14 17:40:00 -0600

    fix the things that were broken 😔

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Rebuilt2026";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 29;
+  public static final String GIT_SHA = "deb4cee32664d7844cd12c098a1ec71c6390bef6";
+  public static final String GIT_DATE = "2026-02-14 18:28:25 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-02-14 18:39:34 EST";
+  public static final long BUILD_UNIX_TIME = 1771112374767L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
--- src/main/java/frc/robot/subsystems/turret/TurretIOReal.java
+++ src/main/java/frc/robot/subsystems/turret/TurretIOReal.java
@@ -8,13 +8,13 @@ import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkClosedLoopController.ArbFFUnits;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.FeedbackSensor;
 import com.revrobotics.spark.SparkAbsoluteEncoder;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkFlexConfig;
-import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 
 import frc.robot.Constants;
 import frc.robot.util.SparkUtil;
