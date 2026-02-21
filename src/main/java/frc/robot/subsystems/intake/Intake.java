89f1aa0803debfabddd529d9b60085b432b8ab42
2826WaveRobotics <wavesoftware@waverobotics.com>
2026-02-20 19:18:09 -0600

    shhh don't tell brody I did this wrong

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -49,7 +49,7 @@ public class Intake extends SubsystemBase {
 
   public Command bringIntakeIn(DoubleSupplier triggerPosition) {
     return setIntakePosition(() -> {
-      return IntakeConstants.fullyInPos + (-IntakeConstants.fullyInPos * triggerPosition.getAsDouble());
+      return IntakeConstants.fullyInPos * triggerPosition.getAsDouble();
     });
   }
 }
