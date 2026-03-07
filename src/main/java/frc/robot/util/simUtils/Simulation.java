9ddae0e07e9e5c6e8008c6310978de1ac679cbbc
Wave Robotics Software <wavesoftware@waverobotics.com>
2026-03-07 15:34:58 -0600

    reenable battery sim

--- src/main/java/frc/robot/util/simUtils/Simulation.java
+++ src/main/java/frc/robot/util/simUtils/Simulation.java
@@ -77,10 +77,11 @@ public final class Simulation {
         for(int i = 0; i < subTicks; i++) {
             fuel.stepSimSubtick();
             driveSimulation.update(simulationDtSeconds);
-            // SimulatedBattery.simulationSubTick();
+            SimulatedBattery.simulationSubTick();
 
             // TODO: also run other sim IO with subticks
             
+               
         }
     }
 
