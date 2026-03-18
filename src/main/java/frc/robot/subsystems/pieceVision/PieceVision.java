31c1412b4364a650c7ea093e8a0ceacd95107d5a
Glitch752 <xxGlitch752xx@gmail.com>
2026-03-17 20:15:41 -0500

    Smal lidk

---
+++ src/main/java/frc/robot/subsystems/pieceVision/PieceVision.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems.pieceVision;
+
+public class PieceVision {
+    
+}
--- src/main/java/frc/robot/util/simUtils/SwerveDriveSimulation.java
+++ src/main/java/frc/robot/util/simUtils/SwerveDriveSimulation.java
@@ -10,7 +10,6 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.units.measure.*;
 import frc.robot.FieldConstants;
-import frc.robot.subsystems.drive.Drive;
 
 import java.util.Arrays;
 import java.util.OptionalDouble;
