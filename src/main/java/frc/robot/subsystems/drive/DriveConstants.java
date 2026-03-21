26bd7658de81b74d03141e022f7085d36197700e
Wave Robotics Software <wavesoftware@waverobotics.com>
2026-03-21 11:34:28 -0500

    Update at 'Sat Mar 21 11:34:28 CDT 2026'

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -193,11 +193,11 @@ public class DriveConstants {
     public static final DCMotor turnMotorModel = DCMotor.getKrakenX60Foc(1);
     
     public static final KinematicConstraints kinematicConstraints = new KinematicConstraints(
-        MetersPerSecondPerSecond.of(12) /* measuered "magic value" - max linear acceleration */,
-        RadiansPerSecondPerSecond.of(10 * (maxAngularSpeedRadPerSec / maxSpeedMetersPerSec)),
-        MetersPerSecondPerSecond.of(9), /* Skid acceleration limit */
-        MetersPerSecondPerSecond.of(7), /* Max tilt acceleration X */
-        MetersPerSecondPerSecond.of(10) /* Max tilt acceleration Y */
+        MetersPerSecondPerSecond.of(24) /* measuered "magic value" - max linear acceleration */,
+        RadiansPerSecondPerSecond.of(20 * (maxAngularSpeedRadPerSec / maxSpeedMetersPerSec)),
+        MetersPerSecondPerSecond.of(18), /* Skid acceleration limit */
+        MetersPerSecondPerSecond.of(14), /* Max tilt acceleration X */
+        MetersPerSecondPerSecond.of(20) /* Max tilt acceleration Y */
     );
     
     // Encoder offsets measured with 
