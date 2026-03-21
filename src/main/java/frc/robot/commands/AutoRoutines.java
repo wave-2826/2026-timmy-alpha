9f536450a7618ec6cbe113f785769d4f91aa7f1a
Glitch752 <xxGlitch752xx@gmail.com>
2026-03-21 09:32:36 -0500

    Add flipped autos to chooser

--- src/main/java/frc/robot/commands/AutoRoutines.java
+++ src/main/java/frc/robot/commands/AutoRoutines.java
@@ -9,10 +9,7 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
 import frc.robot.generated.autos.ChoreoTraj;
-import frc.robot.subsystems.climber.Climber;
 import frc.robot.subsystems.drive.Drive;
-import frc.robot.subsystems.spindexer.Spindexer;
-import frc.robot.subsystems.turret.Turret;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.util.AllianceFlipUtil;
 import frc.robot.util.LoggedAutoChooser;
@@ -41,13 +38,17 @@ public class AutoRoutines {
         // .bind("Outtake", intake.enableOutward());
         
         autoChooser.addRoutine("Left Double Swipe", () -> this.getDoubleSwipe(false));
+        autoChooser.addRoutine("Right Double Swipe", () -> this.getDoubleSwipe(true));
+        autoChooser.addRoutine("Left Sweep Outpost", () -> this.getSweepOutpost(false));
+        autoChooser.addRoutine("Right Sweep Outpost", () -> this.getSweepOutpost(true));
         // autoChooser.addRoutine("Right Double Swipe (fallback)", () -> this.getDoubleSwipe(true));
     }
 
     private AutoRoutine getDoubleSwipe(boolean right) {
-        var routine = autoFactory.newRoutine("LeftDoubleSwipe");
+        var choreoTraj = right ? ChoreoTraj.RightDoubleSwipeGenerated : ChoreoTraj.LeftDoubleSwipe;
+        var routine = autoFactory.newRoutine(choreoTraj.name());
         
-        AutoTrajectory traj = ChoreoTraj.LeftDoubleSwipe.asAutoTraj(routine);
+        AutoTrajectory traj = choreoTraj.asAutoTraj(routine);
 
         traj.atTime("Intake").onTrue(Commands.sequence(intake.deployIntake(), intake.enable()));
         
@@ -56,9 +57,24 @@ public class AutoRoutines {
             traj.cmd()
         ));
 
-        System.out.println("Got routine");
-
         return routine;
     }
 
+    private AutoRoutine getSweepOutpost(boolean right) {
+        var choreoTraj = right ? ChoreoTraj.RightSweepOutpostGenerated : ChoreoTraj.LeftSweepOutpost;
+        var routine = autoFactory.newRoutine(choreoTraj.name());
+        
+        AutoTrajectory traj = choreoTraj.asAutoTraj(routine);
+
+        traj.atTime("Intake").onTrue(Commands.sequence(intake.deployIntake(), intake.enable()));
+        traj.atTime("Intake Stop").onTrue(intake.disable());
+        traj.atTime("Outtake").onTrue(intake.enableOutward());
+        
+        routine.active().onTrue(Commands.sequence(
+            traj.resetOdometry(),
+            traj.cmd()
+        ));
+
+        return routine;
+    }
 }
